16f79035e51c11d94a6b32b36cd88df77f40f2e1
StepanovPlaton <mail2platon@yandex.ru>
2020-05-08 11:01:43 +0400

    Version 1.0

--- run.py
+++ run.py
@@ -2,32 +2,35 @@ import time, random
 
 from modules.SreenReader import *
 from modules.Control import *
-import modules.Neyro
 
 control = СontrolClass(1)
+time.sleep(5)
 
 ScreenReader = ScreenReaderClass((165, 30, 1201, 738))
 
 press_power = 0
 
-while True:
-    x, _ =  ScreenReader.GetRoadMoment()
-    if(abs(x - ((1201-165)/2)) < 200):
-        if(press_power >= 6):
-            control.SetButton(2, 1)
-        else:
-            control.SetButton(2, 0)
-        press_power += 1
-        if(press_power == 11): press_power = 0
-        control.SetButton(1, 0)
-        
-    else:
-        control.SetButton(2, 0)
-        #control.SetButton(1, 1)
+ScreenReader.StartDemon()
 
-    if(x > ((1201-165)/2) and abs(x - ((1201-165)/2)) > 10):
-        control.SetLeftStick(1, -1)
-    elif(x < ((1201-165)/2) and abs(x - ((1201-165)/2)) > 10):
-        control.SetLeftStick(-1, -1)
+while True:
+
+    (x1, _), (x2, _) =  ScreenReader.GetRoadMoment()
+    if(abs(x1 - ((1201-165)/2)) < 110):
+        if(press_power >= 0):  control.SetButton(2, 1) # газ
+        else: control.SetButton(2, 0)
+        press_power += 1
+        if(press_power == 11): press_power = 0 # газ работает 4 из 10
+
+        control.SetButton(1, 0) 
+    elif(abs(x1 - ((1201-165)/2)) < 150):
+        control.SetButton(2, 0)
+    else:
+        control.SetButton(1, 0) 
+
+
+    if(x1 > ((1201-165)/2) and abs(x1 - ((1201-165)/2)) > 60):
+        control.SetLeftStick(1, -1) 
+    elif(x1 < ((1201-165)/2) and abs(x1 - ((1201-165)/2)) > 60):
+        control.SetLeftStick(-1, -1) 
     else:
         control.SetLeftStick(0, 0)
